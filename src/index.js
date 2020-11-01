0d7fddabd16d2e467d0bd2583df8ff2abed75ab8
katerina-prochazkova <kat.prochazkova@gmail.com>
2020-11-01 12:14:13 +0100

    nheader position fixed rozhodí menu

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const navigace = document.getElementById('nav-btn');
+navigace.addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+});
+console.log('jedu!');
